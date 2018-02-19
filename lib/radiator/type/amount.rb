c0d85bf913141cc2e56269125bcc85fc629ef7fa
Chromium <crcs2o4@gmail.com>
2018-02-19 12:00:37 +0100

    ASSET_PRECISION refactoring in Type::Amount

--- lib/radiator/type/amount.rb
+++ lib/radiator/type/amount.rb
@@ -3,22 +3,24 @@ module Radiator
 
     # See: https://github.com/xeroc/piston-lib/blob/34a7525cee119ec9b24a99577ede2d54466fca0e/steembase/operations.py
     class Amount < Serializer
+      ASSET_PRECISION = {
+        'STEEM' => 3,
+        'VESTS' => 6,
+        'SBD' => 3,
+        'GOLOS' => 3,
+        'GESTS' => 6,
+        'GBG' => 3,
+        'CORE' => 3,
+        'CESTS' => 6,
+        'TEST' => 3,
+      }.freeze
+
       def initialize(value)
         super(:amount, value)
 
         @amount, @asset = value.strip.split(' ')
-        @precision = case @asset
-        when 'STEEM' then 3
-        when 'VESTS' then 6
-        when 'SBD' then 3
-        when 'GOLOS' then 3
-        when 'GESTS' then 6
-        when 'GBG' then 3
-        when 'CORE' then 3
-        when 'CESTS' then 6
-        when 'TEST' then 3
-        else; raise TypeError, "Asset #{@asset} unknown."
-        end
+        @precision = ASSET_PRECISION.fetch(@asset, 'unknown')
+        raise TypeError, "Asset #{@asset} unknown." if @asset == 'unknown'
         raise TypeError, "Amount #{@amount} needs to be with #{@precision} exponent" unless @amount.match?(/\.[0-9]{#{@precision}}/)
       end
 
