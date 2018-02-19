c59d6594e1381b14ad8cc1dc7dbe5c28277de338
Chromium <crcs2o4@gmail.com>
2018-02-19 11:54:44 +0100

    add exponent check on Type::Amount

--- lib/radiator/type/amount.rb
+++ lib/radiator/type/amount.rb
@@ -1,11 +1,11 @@
 module Radiator
   module Type
-    
+
     # See: https://github.com/xeroc/piston-lib/blob/34a7525cee119ec9b24a99577ede2d54466fca0e/steembase/operations.py
     class Amount < Serializer
       def initialize(value)
         super(:amount, value)
-        
+
         @amount, @asset = value.strip.split(' ')
         @precision = case @asset
         when 'STEEM' then 3
@@ -19,17 +19,18 @@ module Radiator
         when 'TEST' then 3
         else; raise TypeError, "Asset #{@asset} unknown."
         end
+        raise TypeError, "Amount #{@amount} needs to be with #{@precision} exponent" unless @amount.match?(/\.[0-9]{#{@precision}}/)
       end
-      
+
       def to_bytes
         asset = @asset.ljust(7, "\x00")
         amount = (@amount.to_f * 10 ** @precision).round
-        
+
         [amount].pack('q') +
         [@precision].pack('c') +
         asset
       end
-      
+
       def to_s
         "#{@amount} #{@asset}"
       end
